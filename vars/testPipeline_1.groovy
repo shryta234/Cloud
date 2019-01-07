54d50fc27f2a432eac68f13c269b247b081981b5
Shruti Patnaik <shryta234@gmail.com>
2019-01-08 00:26:25 +0530

    Update testPipeline_1.groovy

--- vars/testPipeline_1.groovy
+++ vars/testPipeline_1.groovy
@@ -6,18 +6,17 @@ def call(body) {
   body()
 
 pipeline {
-agent any
-environment {
-OUTPUT_PATH = './outputs/'
-}
-stages {
-stage ('build') {
-  steps{
-sh 'echo "hi"'
+ agent any
+ parameters {
+  string(name: 'user', defaultValue: 'John', description: 'A user that triggers the pipeline')
+ }
+ stages {
+  stage('Trigger pipeline') {
+   steps {
+    echo "Pipeline triggered by ${params.USER}"
+   }
   }
-                }
-
-      }
+ }
 }
 
 }
