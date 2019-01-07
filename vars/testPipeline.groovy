d103a1569dbbc5936a55a69436a026b594e11c2f
Shruti Patnaik <shryta234@gmail.com>
2019-01-07 23:34:11 +0530

    Update testPipeline.groovy

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,3 +1,11 @@
+def call(body) {
+
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+
 pipeline { 
     agent any  
     stages { 
@@ -8,4 +16,5 @@ pipeline {
             }
         }
     }
+}
 }
