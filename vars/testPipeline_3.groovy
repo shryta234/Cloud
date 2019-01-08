beedf99aa639b7ad06c21f8becf7fde16119c026
Shruti Patnaik <shryta234@gmail.com>
2019-01-08 19:42:00 +0530

    Update testPipeline_3.groovy

--- vars/testPipeline_3.groovy
+++ vars/testPipeline_3.groovy
@@ -10,31 +10,30 @@ def call(body) {
    agent any
    stages{
         stage ('clone source code'){
-            git clone 'https://github.com/shryta234/CloudBees.git'
+		
+		steps{git clone 'https://github.com/shryta234/CloudBees.git'
             bat'del random.txt' 
             bat 'y'
             bat 'dir >> random.txt'
             def name = type random.txt
             if (name == 'CloudBees') { echo 'successfully cloned'}
+			}
+            
 
                                   }
     
-      stage ('compress to .zip'){echo '.zip'}
+      stage ('compress to .zip'){
+				steps{echo '.zip'}
+								}
     
-      stage ('deploy'){echo 'deploy'}
-   
+      stage ('deploy') {
+					steps {echo 'deploy'}
+						}
    
    }
   
   }
   
   
-  
-  
-  
-  
-  
-  
-  
-  
+    
   }
