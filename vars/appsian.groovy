0c40886e66eed8c1147f4ba6fea8d6b4ef445b4a
Shruti Patnaik <shryta234@gmail.com>
2020-11-07 16:21:50 +0530

    Update appsian.groovy

--- vars/appsian.groovy
+++ vars/appsian.groovy
@@ -1,4 +1,9 @@
 
+def call(body) {
+def config = [:]
+body.resolveStrategy = Closure.DELEGATE_FIRST
+body.delegate = config
+body()
 pipeline {
    agent any
    stages{
@@ -24,3 +29,4 @@ pipeline {
   
            }
 
+}
