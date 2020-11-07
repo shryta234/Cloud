c281bfcb6a43b0811f0ab2afac9cd3798b6643a4
Shruti Patnaik <shryta234@gmail.com>
2020-11-07 16:27:20 +0530

    Update appsian.groovy

--- vars/appsian.groovy
+++ vars/appsian.groovy
@@ -6,23 +6,30 @@ body.delegate = config
 body()
 pipeline {
    agent any
+	environment {
+        DISABLE_AUTH = 'qwerty'
+        DB_ENGINE    = 'sqlite'
+ 
+                    }
    stages{
         stage ('clone source code'){		
 				steps{
 	          echo 'Appsian cloned source code'
+		 echo "${DB_ENGINE}"
 				     }
                                    }
     
       stage ('compress to .zip'){
 				steps{
 					echo 'Appsian .zip'
+					echo "${DISABLE_AUTH}"
 				     }
-				                        }    
+				}    
       stage ('deploy') {
-				steps {
-					echo 'Appsian deployed'
-				      }
-			                  }
+			steps {
+				echo 'Appsian deployed'
+			      }
+		       }
    
           
           }
