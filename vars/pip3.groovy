4e284efbfe11bd599368135a383658d8885eadc9
Shruti Patnaik <shryta234@gmail.com>
2020-12-04 20:16:07 +0530

    Update pip3.groovy

--- vars/pip3.groovy
+++ vars/pip3.groovy
@@ -1,3 +1,9 @@
+def call(body) {
+def config = [:]
+body.resolveStrategy = Closure.DELEGATE_FIRST
+body.delegate = config
+body()
+
 pipeline {
     agent any
     stages {
@@ -15,3 +21,4 @@ pipeline {
           
     } //stages
 } //pipeline
+}
